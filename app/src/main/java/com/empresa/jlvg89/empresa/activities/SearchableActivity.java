1064a1e943c772e6d5db071fd0e61903636e11e4
Joao Gouveia <j.lucas.gouveia@gmail.com>
2018-03-03 00:06:20 -0300

    search flow is working

--- app/src/main/java/com/empresa/jlvg89/empresa/activities/SearchableActivity.java
+++ app/src/main/java/com/empresa/jlvg89/empresa/activities/SearchableActivity.java
@@ -3,38 +3,59 @@ package com.empresa.jlvg89.empresa.activities;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.empresa.jlvg89.empresa.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SearchableActivity extends AppCompatActivity {
+
+    private static final String TAG = "SearchableActivity";
 
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_searchable);
 
-        if(!checkUserCredentials()){
-            redirectToLogin();
-        }
         initializeWidgets();
         setSearchActionBar(toolbar);
+
+        handleSearch(getIntent());
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+        handleSearch(intent);
+    }
 
+    public void handleSearch(Intent intent){
+        if(Intent.ACTION_SEARCH.equalsIgnoreCase(intent.getAction())){
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.i(TAG, query);
+        }
+    }
+
+    private void setSearchActionBar(Toolbar toolbar) {
+        setSupportActionBar(toolbar);
+    }
+
+    private void initializeWidgets() {
+        toolbar = (Toolbar) findViewById(R.id.tb_searchable_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_searchable_activity, menu);
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchView searchView;
         MenuItem item = menu.findItem(R.id.action_searchable_activity);
@@ -47,40 +68,6 @@ public class MainActivity extends AppCompatActivity {
 
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setQueryHint(getString(R.string.search_hint));
-
         return true;
-    }
-
-    private void setSearchActionBar(Toolbar toolbar) {
-        setSupportActionBar(toolbar);
-    }
-
-    private void initializeWidgets() {
-        toolbar = (Toolbar) findViewById(R.id.tb_search);
-    }
-
-
-    /**
-     * REDIRECIONA PARA A TELA DE LOGIN
-     */
-    private void redirectToLogin() {
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    /**
-     * VERIFICA SE O USUÁRIO FEZ O LOGIN
-     * @return
-     */
-    private boolean checkUserCredentials() {
-        SharedPreferences preferences = getSharedPreferences("empresas", MODE_PRIVATE);
-
-        if(preferences.getString("access-token", null) != null &&
-                preferences.getString("client", null) != null &&
-                preferences.getString("uid", null) != null){
-            return true;
-        }
-        return false;
     }
 }
